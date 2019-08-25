f4111fe98e0046a29a16b06c8f0117ae6d866e47
barat <barat@DESKTOP-ACER>
2019-08-25 11:23:46 +0800

    Initial Commit

---
+++ eg-innovations-rest/src/main/java/org/grassfield/egcli/EgInnovationsRestApplication.java
@@ -0,0 +1,13 @@
+package org.grassfield.egcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgInnovationsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgInnovationsRestApplication.class, args);
+	}
+
+}
